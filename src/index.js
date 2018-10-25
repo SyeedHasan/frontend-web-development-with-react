6a20f948dc39a15db41a2ed492d502cc65176dff
Hasan <naqvi_hasan@live.com>
2018-10-25 18:31:36 +0500

    Fixed conflicts in merge

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
-=======
-import 'bootstrap/dist/css/bootstrap.css';
->>>>>>> a650f05596fd1194c4c2d87c20dc7884dc01fbf1
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
