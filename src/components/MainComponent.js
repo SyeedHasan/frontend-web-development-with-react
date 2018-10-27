3270364328222cec8a605c5833d2596fa32258ba
Hasan <naqvi_hasan@live.com>
2018-10-27 19:12:06 +0500

    React Router

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,20 +4,15 @@ import { DISHES } from '../shared/dishes';
 import Header from './HeaderComponent';
 import Dish from './DishdetailComponent';
 import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
       dishes: DISHES,
-      selectedDish: null
     }
-  }
-
-  onDishSelect(dishId) {
-    this.setState({
-      selectedDish: dishId
-    });
   }
 
   sendDish() {
@@ -28,12 +23,24 @@ class Main extends Component {
   }
 
   render() {
+
+    const HomePage = () => {
+      return (
+        <Home />
+      );
+    }
+
     return (
       <div className="App">
         <Header />
-        <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
+        <Switch>
+          <Route path="/home" component={Home} />
+          <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+          <Redirect to="/home" />
+        </Switch>
+        {/* <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} /> */}
         {/* Selecting specific dish by fitlering the main array and get one result b/c id's are unique */}
-        <Dish dish={this.sendDish()} />
+        {/* <Dish dish={this.sendDish()} /> */}
         <Footer />
       </div>
     );
